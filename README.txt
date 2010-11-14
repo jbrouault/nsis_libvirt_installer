85ce711d2d1f5761a63f1f665a717349e31cc94a
Justin Clift <jclift@redhat.com>
2010-11-14 12:18:08 +1100

    Added initial README file and license files thus far.

---
+++ README.txt
@@ -0,0 +1,21 @@
+These are the packaging pieces, used in creating the libvirt Windows installer.
+
+Caveats
+*******
+
+ + This is compiled in 32-bit mode only, due to the use of msys.  It should
+   work on both 32-bit and 64-bit versions of Windows.
+
+ + This is a fairly new effort, so is still in fairly rough form.  Feel
+   welcome to try things out, but it's definitely not yet "production"
+   grade (unlike libvirt itself)
+
+ + We're very interested in bug reports and suggestions for improvement,
+   so please feel free to get in contact with us on the libvirt mailing
+   list:
+
+     http://libvirt.org/contact.html
+
+   Or email Justin Clift <jclift@redhat.com> directly if that's easier.
+   (mailing list preferred though)
+
---
+++ licenses/libxml2-2.dll/trionan.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+ *
+ * $Id$
+ *
+ * Copyright (C) 2001 Bjorn Reese <breese@users.sourceforge.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE AUTHORS AND
+ * CONTRIBUTORS ACCEPT NO RESPONSIBILITY IN ANY CONCEIVABLE MANNER.
+ *
+************************************************************************/
